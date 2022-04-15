04328cceb0b334620d5122c8abb0abe2417d20dd
brayohmurithidev <murithibrianm@gmail.com>
2022-04-15 23:02:38 +0300

    initial commit

---
+++ brayoh/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import PortfolioContainer from './portfolioContainer/PortfolioContainer';
+
+
+function App() {
+  return (
+    <div className="App">
+      <PortfolioContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ brayoh/src/portfolioContainer/Home/Profile/Profile.js
@@ -0,0 +1,77 @@
+import React from "react";
+import Typical from "react-typical";
+import "./profile.css";
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <div className="colz-icon">
+              <a
+                href="https://www.facebook.com/Deejay-Dephaz-325353291351524"
+                target="_blank"
+              >
+                <i className="fa fa-facebook-square"></i>
+              </a>
+              <a href="#">
+                <i className="fa fa-twitter"></i>
+              </a>
+              <a href="https://github.com/brayohmurithidev" target="_blank" >
+                <i className="fa fa-github"></i>
+              </a>
+              <a href="https://www.linkedin.com/in/brian-murithi/" target="_blank">
+                <i className="fa fa-linkedin"></i>
+              </a>
+            </div>
+          </div>
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {""}
+              Hello, I'M <span className="highlighted-text">Brian Murithi</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {""}
+              <h1>
+                {""}
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Enthusiatstic Dev 🛑",
+                    1000,
+                    "Full Stack Developer 💻",
+                    1000,
+                    "Mern Stack Dev 😎",
+                    1000,
+                    "Cross Platform V 🕸️",
+                    1000,
+                    "React Dev 📱",
+                    1000,
+                  ]}
+                />
+              </h1>
+              <span className="profile-role-tagline">
+                 Passionate in building application with front and back end operations.
+              </span>
+            </span>
+          </div>
+          <div className="profile-options">
+            <button className="btn primary-btn">
+              {""}
+              Hire Me{" "}
+            </button>
+            <a href="bdephaz.pdf" download="Bdephaz bdephaz.pdf">
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+          </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
